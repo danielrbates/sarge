2e832797f7289c8de366bda416437e4f9f75d121
Daniel Bates <danielrbates@gmail.com>
2019-02-19 09:39:01 -0500

    Bugfixes.
    Fixed a large number of errors related to lack of variable quoting in echo statements.

--- NMAP - list hosts by open ports.sh
+++ NMAP - list hosts by open ports.sh
@@ -4,7 +4,7 @@
 FilePath=$PWD
 InputFile="NMAP_all_hosts.txt"
 OutputFile="${FilePath}/Enumerated_NMAP.txt"
-:> $OutputFile
+:>| $OutputFile
 
 # Import text from file.
 FullText=$(cat $FilePath/$InputFile)
@@ -13,7 +13,7 @@ FullText=$(cat $FilePath/$InputFile)
 # to a variable named $ScanReports.  (Note: we will replace all newlines with
 # #NEWLINE# to enable grep to find patterns across multiple lines of input.)
 ScanReports=$(
-	cat $FullText | sed 's/$/#NEWLINE#/' | tr -d '\n' | \
+	echo "$FullText" | sed 's/$/#NEWLINE#/' | tr -d '\n' | \
 	grep -Po '(Nmap scan report for).*?(Network Distance: \d+ hops?)'
 	)
 
@@ -21,25 +21,25 @@ ScanReports=$(
 # service names.
 OrderedPorts=$(
     echo "$FullText" | grep -Po '^(\d{1,5}\/(tcp|udp))\s+(open)\s+(\S+)$' | \
-    awk '{print $1 $3}' | sort -n | uniq
+    awk '{print $1 " " $3}' | sort -n | uniq
     )
 
 # Define a function that will look up a service name, given a port.
 lookup_service () {
-    echo "$OrderedPorts" | grep -P "^$1" | awk '{print $3}'
+    echo "$OrderedPorts" | grep -P "^$1" | awk '{print $2}'
     }
 
 # Define a function that will count the number of instances of a given open
 # port within the Nmap output.
 count_hosts_by_port () {
-    echo $FullText | grep -Po "^($1)\s+(open)" | wc -l
+    echo "$FullText" | grep -Po "^($1)\s+(open)" | wc -l
     }
 
 # Define a function that will take a port, grep for the preceding IP address
 # (if one exists before the start of the scan report block), and return the
 # IP address within each match.
 list_hosts_by_port () {
-    echo $ScanReports | \
+    echo "$ScanReports" | \
     grep -Po "(?<=scan report for )(?:.(?!scan report))*(#)($1)(\s+open)" | \
     grep -Po '(\d{1,3}\.){3}\d{1,3}'
     }
